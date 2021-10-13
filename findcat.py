7bb38fd2d9ad31d965bb7650092faaa3484ee6e1
lala <90055383+befreshyo@users.noreply.github.com>
2021-10-12 20:43:30 -0500

    Create findcat.py

---
+++ findcat.py
@@ -0,0 +1,33 @@
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+import os
+import webbrowser
+
+os.chdir = input("enter directory: ")
+catImage = Image.open(input("enter image name: "))
+
+
+catExifData = catImage._getexif()
+catGPSData = catExifData[34853]
+
+lat = catGPSData[2]
+lon = catGPSData[4]
+
+def dmsToDecimal(degTriple):
+    return degTriple[0] + degTriple[1]/60.0 + degTriple[2]/3600.0
+
+latDec = dmsToDecimal(lat)
+lonDec = dmsToDecimal(lon)
+
+if catGPSData[1] == 'S':
+    latDec = -latDec
+
+if catGPSData[3] == 'W':
+    lonDec = -lonDec
+
+print("lat: ",latDec)
+
+print(latDec," , ",lonDec)
+locString=str(latDec)+','+str(lonDec)
+mapURL = 'https://www.google.com/maps/place/' + locString
+webbrowser.open(mapURL)
